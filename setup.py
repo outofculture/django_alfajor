b104bbed4cf160b05826014ff3669e021cca7374
Dan <dcolish@gmail.com>
2010-09-10 15:30:57 -0700

    fixes to setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(name="django_alfajor",
+      version="0.1",
+      packages=find_packages(),
+      author='',
+      author_email='',
+      description='',
+      keywords='testing test functional integration browser ajax selenium',
+      long_description='moo',
+      license='BSD',
+      url='http://http://github.com/outofculture/django_alfajor',  # FIXME
+
+      classifiers=[
+          'Development Status :: 1 - Planning',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Internet :: WWW/HTTP :: Browsers',
+          'Topic :: Software Development :: Testing',
+          'Topic :: Software Development :: Quality Assurance',
+          ],
+      )
